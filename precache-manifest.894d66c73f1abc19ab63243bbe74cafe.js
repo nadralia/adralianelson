72a3076306d6ac20b217fdce9a0cd596943bf8d4
Adralia Nelson Mandela <adralianelsonmandela@Adralias-MacBook-Pro.local>
2020-07-11 17:17:02 +0300

    Updates

---
+++ precache-manifest.894d66c73f1abc19ab63243bbe74cafe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee67d63a434b954be9b871dc33ab6d4",
+    "url": "/adralianelson/index.html"
+  },
+  {
+    "revision": "c2113ec259aced048d4e",
+    "url": "/adralianelson/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a4d1b1eb5be579756b4b",
+    "url": "/adralianelson/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "c2113ec259aced048d4e",
+    "url": "/adralianelson/static/js/2.d57e30b8.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/adralianelson/static/js/2.d57e30b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d1b1eb5be579756b4b",
+    "url": "/adralianelson/static/js/main.79f1360a.chunk.js"
+  },
+  {
+    "revision": "6bcd723066c4eab353a0",
+    "url": "/adralianelson/static/js/runtime-main.3ab793a5.js"
+  },
+  {
+    "revision": "01130b7bb783483feb9c4caab9ce4fef",
+    "url": "/adralianelson/static/media/resume.01130b7b.pdf"
+  }
+]);
